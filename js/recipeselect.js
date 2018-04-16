4631a7385386d544c969fc00490175adc8952ff5
Danny Toback <drt008@bucknell.edu>
2018-04-16 11:53:25 -0400

    the stupid thing

--- js/recipeselect.js
+++ js/recipeselect.js
@@ -1,17 +1,46 @@
 function loadAjax(){
-  var autoCompTextURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/parseIngredients?includeNutrition=false";
+  var textURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/queries/analyze";
   var myIngredient = document.getElementById("ingredient").value;
-  console.log("making fetch to", autoCompTextURL)
 
-  fetch(autoCompTextURL, {
-    body: "ingredientList="+myIngredient+"&servings=2",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded",
-      "X-Mashape-Key": "kUvc4U4wx8mshla6aUVHG3KdK5oIp1ZyIDsjsn2PGSErYa4kl1"
-    },
-    method: "POST"
-  })
-}
+  const APIKEY = "kUvc4U4wx8mshla6aUVHG3KdK5oIp1ZyIDsjsn2PGSErYa4kl1"
+  const PROJECTNAME = "FridgeRaider"
+  const RapidAPI = new require('rapidapi-connect');
+  const rapid = new RapidAPI(PROJECTNAME, APIKEY);
 
-document.addEventListener("DOMContentLoaded", loadAjax)
+
+  // These code snippets use an open-source library. http://unirest.io/nodejs
+fetch(textURL, {
+.header("Accept", "application/json")
+.header()
+})
+  console.log(result.status, result.headers, result.body);
+});
+
+
+
+
+//
+//   console.log("making fetch to", autoCompTextURL)
+//   console.log("Got incredient as", myIngredient)
+//   fetch(autoCompTextURL, {
+//     body: "ingredientList="+myIngredient+"&servings=2",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/x-www-form-urlencoded",
+//       "X-Mashape-Key": "kUvc4U4wx8mshla6aUVHG3KdK5oIp1ZyIDsjsn2PGSErYa4kl1"
+//     },
+//     method: "POST"
+//   })
+//   .then(resp=>{
+//   resp.json()
+//     .then(json => {
+//       console.log(json)
+//       document.getElementByID('new_ingredient')
+//         .src=json.message
+//         console.log(document.getElementById('new_ingredient'))
+//     })
+//     .catch()
+//   })
+// .catch(error => console.log("ERROR" + error))
+// }
+// document.addEventListener("DOMContentLoaded", loadAjax)
